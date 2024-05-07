e86c35875b6e895999d8526370cc4b7840a413b4
Uzair Ahmed <uzairpm@gmail.com>
2024-05-07 21:51:33 +0400

    Nextjs base, Clerk integrated

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Sidebar from '@/components/Sidebar';
+import Navbar from '@/components/Navbar';
+
+const HomeLayout = ({children}: { children: React.ReactNode }) => {
+  return (
+    <main className='relative'>
+        <Navbar />
+        <div className='flex'>
+            <Sidebar />
+
+            <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
+                <div className='w-full'>
+                    {children}
+                </div>
+            </section>
+        </div>
+    </main>
+  )
+}
+
+export default HomeLayout
