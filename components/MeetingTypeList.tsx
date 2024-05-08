6d91c867c91e0a7bb99c9c094c4ff8b3e058163a
Uzair Ahmed <uzairpm@gmail.com>
2024-05-08 20:45:50 +0400

    Integrate stream-io sdk

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,17 +1,62 @@
-'use client'
+'use client';
 
 import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
 import HomeCard from './HomeCard'
-import { useRouter } from 'next/router'
 import MeetingModal from './MeetingModal'
+import { useUser } from '@clerk/nextjs'
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
+import Loader from './Loader';
+
+const initialValues = {
+    dateTime: new Date(),
+    description: '',
+    link: '',
+};
 
 const MeetingTypeList = () => {
-//   const router = useRouter();
+  const router = useRouter();
   const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>();
 
-  const createMeeting = () => {
-    
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+  const [values, setValues] = useState(initialValues);
+  const [callDetails, setCallDetails] = useState<Call>();
+
+  const createMeeting = async () => {
+    if (!client || !user)  return;
+
+    try {
+        const id = crypto.randomUUID();
+        const call = client.call('default', id);
+
+        if (!call) throw new Error('Failed to create call');
+
+        const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+        const description = values.description || 'Instant meeting';
+
+        await call.getOrCreate({
+            data: {
+                starts_at: startsAt,
+                custom: {
+                    description
+                }
+            }
+        });
+
+        setCallDetails(call);
+
+        if (!values.description) {
+            router.push(`/meeting/${call.id}`);
+        }
+
+    } catch(error) {
+        console.log(error);
+    }
   }
+
+  if (!client || !user) return <Loader />;
 
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
